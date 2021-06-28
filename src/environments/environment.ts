693d83f973f3d9d8bb37d3458c9fa01bc852c422
Jaela-C <jaelacarlosama3c@hotmail.com>
2021-06-28 15:23:25 -0500

    Página de inicio de sesión

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase : {
+    apiKey: "AIzaSyBmwyIAILEIciWGKr7Y4-ir_R_QLC45ymE",
+    authDomain: "fundaciones-quito.firebaseapp.com",
+    databaseURL: "https://fundaciones-quito-default-rtdb.firebaseio.com",
+    projectId: "fundaciones-quito",
+    storageBucket: "fundaciones-quito.appspot.com",
+    messagingSenderId: "434057459017",
+    appId: "1:434057459017:web:17662afb190fa49dd49698",
+    measurementId: "G-1NQ1Y66S00"
+  },
 };
 
 /*
