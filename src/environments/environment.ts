d5c0fddb09025fbcde6d0897c743be9093e63ba1
Jaela-C <66124353+Jaela-C@users.noreply.github.com>
2022-03-22 10:16:56 -0500

    Credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,13 @@ export const environment = {
   production: false,
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   firebase : {
-    apiKey: "AIzaSyBmwyIAILEIciWGKr7Y4-ir_R_QLC45ymE",
-    authDomain: "fundaciones-quito.firebaseapp.com",
-    databaseURL: "https://fundaciones-quito-default-rtdb.firebaseio.com",
-    projectId: "fundaciones-quito",
-    storageBucket: "fundaciones-quito.appspot.com",
-    messagingSenderId: "434057459017",
-    appId: "1:434057459017:web:17662afb190fa49dd49698",
-    measurementId: "G-1NQ1Y66S00"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   },
 };
 
